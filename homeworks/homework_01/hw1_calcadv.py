a1e4378216d1a5134d0d54ed836b3988e38bd254
LyudmilaTretyakova <lyudmila_1997@list.ru>
2019-03-07 02:39:58 +0300

    ДЗ_1_Третьякова_Людмила

--- homeworks/homework_01/hw1_calcadv.py
+++ homeworks/homework_01/hw1_calcadv.py
@@ -11,3 +11,35 @@ def advanced_calculator(input_string):
     :return: результат выполнение операции, если строка валидная - иначе None
     '''
     raise NotImplementedError
+#   у меня конечно не получилось, но если будете проверять знайте,
+#   что я пыталась :)
+#   import operator
+# import re
+# ops = { '+': operator.add,        '-': operator.sub,  '*': operator.mul,
+#       '/': operator.truediv}
+# def eval_expression(tokens, stack):
+#     _rex=re.compile(r'^[-+]?[0-9]*[.,]?[0-9]+(?:[eE][-+]?[0-9]+)?$')
+#     for token in tokens:
+#         if _rex.match(token):
+#             stack.append(float(token))
+#         elif token in ops:
+#             if len(stack) < 1:
+#                 return None
+#             elif len(stack)==1:
+#                 return stack.pop()
+#             else:
+#                 a = stack.pop()
+#                 b = stack.pop()
+#             op = ops[token]
+#             stack.append(op(b,a))
+#         else:
+#             return None
+#     return stack.pop()
+#
+# def advanced_calculator (expr):
+#     expression=expr
+#     stack = []
+#     if len(expression)==0:
+#             return None
+#     else:
+#         return eval_expression(expression.split(' '), stack)
