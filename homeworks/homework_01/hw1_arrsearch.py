c7be705ca0ff3d9965e909461a22e46043ed483d
LyudmilaTretyakova <lyudmila_1997@list.ru>
2019-03-12 00:06:43 +0300

    done arrsearch

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -1,16 +1,33 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
 def find_indices(input_list, n):
-    '''
-    Метод возвращает индексы двух различных
-    элементов listа, таких, что сумма этих элементов равна
-    n. В случае, если таких элементов в массиве нет,
-    то возвращается None
-    Ограничение по времени O(n)
-    :param input_list: список произвольной длины целых чисел
-    :param n: целевая сумма
-    :return: tuple из двух индексов или None
-    '''
-    raise NotImplementedError
+    global f, sec
+    d = dict()
+    if len(input_list) > 1:
+        for position, value in enumerate(input_list):
+            raznost = n - value
+            d[raznost] = position
+        for j, val in enumerate(input_list):
+            s = d.get(val, -1)
+            if j == s:
+                continue
+            else:
+                if s != -1:
+                    if isinstance(s, list):
+                        f = s[0]
+                        sec = s[1]
+                        break
+                    else:
+                        f = s
+                        s = d.get(n - val, -1)
+                        if isinstance(s, list):
+                            sec = s[0]
+                        else:
+                            sec = s
+                        break
+                else:
+                    continue
+        if s == -1:
+            return None
+        else:
+            return f, sec
+    else:
+        return None
