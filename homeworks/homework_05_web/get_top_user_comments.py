3853f4c7d70006ac7dc95219db975e056cbe7cae
LyudmilaTretyakova <lyudmila_1997@list.ru>
2019-03-31 20:20:08 +0300

    HW_5web

--- homeworks/homework_05_web/get_top_user_comments.py
+++ homeworks/homework_05_web/get_top_user_comments.py
@@ -1,9 +1,51 @@
 import sys
 
 # Ваши импорты
+import asyncio
+import requests
+from bs4 import BeautifulSoup
+import collections
+import csv
+
+
+@asyncio.coroutine
+def kinks(link):
+    jk = []
+    r = requests.get(link)
+    soup = BeautifulSoup(r.text, 'html5lib')
+    zz = []
+    z = soup.findAll("span", attrs={"class":
+                                    "user-info__nickname user-info__nickname_small user-info__nickname_comment"})
+    x = len(z)
+    if x < 100:
+        return 0
+    else:
+        for i in range(x):
+            zz.append(z[i].contents[0])
+        a = collections.Counter(zz).most_common()
+        s = []
+        a.sort(key=lambda x: (-x[1], x[0]))
+        for i in range(len(a)):
+            jk.append(link)
+            jk.append(a[i][0])
+            jk.append(a[i][1])
+            s.append(jk)
+            jk = []
+        with open('top_user_comments.csv', 'a', newline="") as fp:
+            writer = csv.writer(fp)
+            writer.writerows(s)
+
 
 if __name__ == '__main__':
     filename = 'top_user_comments.csv'
+    open(filename, 'w').close()
     links = sys.argv[1:4]
-
-    # Ваш код
+    lop = asyncio.get_event_loop()
+    task = [
+        asyncio.ensure_future(kinks(links[0])),
+        asyncio.ensure_future(kinks(links[1])),
+        asyncio.ensure_future(kinks(links[2]))
+    ]
+    wait = asyncio.wait(task)
+    lop.run_until_complete(wait)
+    lop.close()
